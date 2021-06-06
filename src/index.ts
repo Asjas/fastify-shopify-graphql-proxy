68f6349bd87bbfd26f3350ec64d9c51926c76239
A-J Roos <asjas@hey.com>
2021-06-06 16:44:38 +0200

    fix bug in checking for accesstoken or shop value

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,36 @@
-import proxy from 'fastify-reply-from';
-import type { FastifyInstance } from 'fastify';
-import { IncomingHttpHeaders } from 'http';
-import { ApiVersion, ShopifySession, ProxyOptions } from './types';
+import proxy from "fastify-reply-from";
+import type { FastifyInstance } from "fastify";
+import { IncomingHttpHeaders } from "http";
+import { ApiVersion, ShopifySession, ProxyOptions } from "./types";
 
 export default async function shopifyGraphQLProxy(fastify: FastifyInstance, proxyOptions: ProxyOptions) {
-  const session: ShopifySession = { shop: '', accessToken: '' };
+  const session: ShopifySession = { shop: "", accessToken: "" };
 
-  fastify.addHook('onRequest', async (request) => {
+  fastify.addHook("onRequest", (request, _reply, done) => {
     session.shop = request?.session?.shop;
     session.accessToken = request?.session?.accessToken;
+    done();
   });
 
-  const shop = 'shop' in proxyOptions ? proxyOptions.shop : session.shop;
-  const accessToken = 'password' in proxyOptions ? proxyOptions.password : session.accessToken;
+  const shop = "shop" in proxyOptions ? proxyOptions.shop : (session.shop as string);
+  const accessToken = "password" in proxyOptions ? proxyOptions.password : (session.accessToken as string);
   const version = proxyOptions.version || ApiVersion.Stable;
 
-  if (accessToken === undefined || shop === undefined) {
-    throw new Error('Unauthorized');
+  if (accessToken === "" || shop === "") {
+    throw new Error("Unauthorized, Shopify `accessToken` or `shop` arguments are empty.");
   }
 
-  fastify.register(proxy, {
+  await fastify.register(proxy, {
     base: shop,
   });
 
-  fastify.post('/graphql', function(_request, reply) {
+  fastify.post("/graphql", (_request, reply) => {
     reply.from(`${shop}/admin/api/${version}/graphql.json`, {
       rewriteRequestHeaders(_originalReq, headers) {
         const modifiedHeaders = {
           ...headers,
-          'Content-Type': 'application/json',
-          'X-Shopify-Access-Token': accessToken,
+          "Content-Type": "application/json",
+          "X-Shopify-Access-Token": accessToken,
         } as IncomingHttpHeaders;
 
         return modifiedHeaders;
