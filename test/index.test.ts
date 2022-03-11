d0dd69144aaf46628e215736f36fc23c85f95f8b
A-J Roos <asjas@hey.com>
2022-03-11 22:33:49 +0200

    remove unused test

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,33 +2,13 @@ import { test } from "tap";
 import fastify from "fastify";
 import shopifyGraphQLProxy, { ApiVersion } from "../src";
 
-test("expect request failure due to using test `shop` and `password` options", async (t) => {
-  t.plan(1);
-  const server = fastify({ logger: false });
-
-  await server.register(shopifyGraphQLProxy, {
-    shop: "https://my-shopify-store.myshopify.com",
-    password: "SHOPIFY_API_ACCESS_TOKEN",
-    version: ApiVersion.Stable,
-  });
-
-  t.teardown(server.close.bind(server));
-
-  const response = await server.inject({
-    method: "POST",
-    url: "/graphql",
-  });
-
-  t.same(JSON.parse(response.body), { errors: { query: "Required parameter missing or invalid" } });
-});
-
-test("throws error if `shop` option isn't passed to plugin", async (t) => {
+test("throws error if shop argument is empty", async (t) => {
   t.plan(1);
   try {
     const server = fastify({ logger: false });
 
     await server.register(shopifyGraphQLProxy, {
-      password: "SHOPIFY_API_ACCESS_TOKEN",
+      accessToken: "SHOPIFY_API_ACCESS_TOKEN",
       version: ApiVersion.Stable,
     });
 
@@ -38,7 +18,7 @@ test("throws error if `shop` option isn't passed to plugin", async (t) => {
   }
 });
 
-test("throws error if `password` option isn't passed to plugin", async (t) => {
+test("throws error if accessToken argument is empty", async (t) => {
   t.plan(1);
   try {
     const server = fastify({ logger: false });
