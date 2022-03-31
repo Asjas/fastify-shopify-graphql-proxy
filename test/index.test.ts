f973327b53101c0452cf9126d12f6ea66731f6a5
A-J Roos <asjas@hey.com>
2022-03-31 13:40:17 +0200

    replace node-tap with vitest and add more tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,36 +1,102 @@
-// @ts-ignore
-import { test } from "tap";
+import { describe, test, assert, expect } from "vitest";
+import { MockAgent, setGlobalDispatcher } from "undici";
 import fastify from "fastify";
-import shopifyGraphQLProxy, { ApiVersion } from "../src";
+import shopifyGraphQLProxy from "../src";
+import { ApiVersion } from "../src/types";
 
-test("throws error if shop argument is empty", async (t) => {
-  t.plan(1);
-  try {
-    const server = fastify({ logger: false });
+const mockAgent = new MockAgent({ connections: 1 });
 
-    await server.register(shopifyGraphQLProxy, {
-      accessToken: "SHOPIFY_API_ACCESS_TOKEN",
-      version: ApiVersion.Stable,
-    });
+setGlobalDispatcher(mockAgent);
 
-    t.teardown(server.close.bind(server));
-  } catch (err: any) {
-    t.equal(err.message, "Unauthorized, Shopify `accessToken` or `shop` arguments are empty.");
-  }
-});
+describe("shopifyGraphQLProxy", () => {
+  test("should throw error if shop argument is empty", async () => {
+    try {
+      const server = fastify({ logger: false });
 
-test("throws error if accessToken argument is empty", async (t) => {
-  t.plan(1);
-  try {
+      await server.register(shopifyGraphQLProxy, {
+        accessToken: "SHOPIFY_API_ACCESS_TOKEN",
+        version: ApiVersion.Stable,
+      });
+    } catch (err: any) {
+      assert.equal(err.message, "Unauthorized, shopifyGraphQLProxy `shop` or `accessToken` arguments are empty.");
+    }
+  });
+
+  test("should throw error if accessToken argument is empty", async () => {
+    try {
+      const server = fastify({ logger: false });
+
+      await server.register(shopifyGraphQLProxy, {
+        shop: "https://my-shopify-store.myshopify.com",
+        version: ApiVersion.Stable,
+      });
+    } catch (err: any) {
+      assert.equal(err.message, "Unauthorized, shopifyGraphQLProxy `shop` or `accessToken` arguments are empty.");
+    }
+  });
+
+  test("should send query to custom shopifyGraphQLProxy prefix", async () => {
+    const mockPool = mockAgent.get("http://127.0.0.1:3001");
+
+    mockPool.intercept({ path: `/admin/api/${ApiVersion.Stable}/graphql.json`, method: "POST" }).reply(
+      200,
+      {
+        data: { shopify: "data" },
+      },
+      { headers: { "content-type": "application/json" } },
+    );
+
     const server = fastify({ logger: false });
 
     await server.register(shopifyGraphQLProxy, {
       shop: "https://my-shopify-store.myshopify.com",
+      accessToken: "SHOPIFY_API_ACCESS_TOKEN",
       version: ApiVersion.Stable,
+      prefix: "/shopify",
+      undici: mockPool,
     });
 
-    t.teardown(server.close.bind(server));
-  } catch (err: any) {
-    t.equal(err.message, "Unauthorized, Shopify `accessToken` or `shop` arguments are empty.");
-  }
+    await server.listen(3001);
+
+    const response = await server.inject({ method: "POST", url: "/shopify/graphql", payload: { some: "data" } });
+
+    assert.equal(response.statusCode, 200);
+    expect(response.json()).toMatchObject({
+      data: { shopify: "data" },
+    });
+
+    await server.close();
+  });
+
+  test("should send query to Shopify GraphQL API", async () => {
+    const mockPool = mockAgent.get("http://127.0.0.1:3000");
+
+    mockPool.intercept({ path: `/admin/api/${ApiVersion.Stable}/graphql.json`, method: "POST" }).reply(
+      200,
+      {
+        data: { shopify: "data" },
+      },
+      { headers: { "content-type": "application/json" } },
+    );
+
+    const server = fastify({ logger: false });
+
+    await server.register(shopifyGraphQLProxy, {
+      shop: "https://my-shopify-store.myshopify.com",
+      accessToken: "SOME_FAKE_TOKEN",
+      version: ApiVersion.Stable,
+      undici: mockPool,
+    });
+
+    await server.listen(3000);
+
+    const response = await server.inject({ method: "POST", url: "/graphql", payload: { some: "data" } });
+
+    assert.equal(response.statusCode, 200);
+    expect(response.json()).toMatchObject({
+      data: { shopify: "data" },
+    });
+
+    await server.close();
+  });
 });
